df46a21b12469b1637b2fffe90674326c92c9358
sunfeilong <sunfeilong1993@gmail.com>
2019-05-26 23:11:38 -0400

    调整脚本错误

--- software/docker/install_docker.sh
+++ software/docker/install_docker.sh
@@ -12,8 +12,8 @@ yum remove docker docker-client docker-client-latest docker-common docker-latest
 # install  
 # install docker-ce
 yum install -y device-mapper-persistent-data lvm2
-yum-config-mamanger --add-repo https://download.docker.com/linux/centos/docker-ce.repoyum-config-manager 
-yum install docker-ce
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
 
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -25,4 +25,5 @@ echo "==================="
 echo "${description} end at   " $(date +"%Y-%m-%d %H:%M:%S")
 echo "${description} cost_time:"$cost_time"s"
 echo "leave the: directory $(pwd)"
+
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
