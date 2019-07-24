44928d9af6d50c4059e7a06d418f673023c33187
sunfeilong <sunfeilong1993@gmail.com>
2019-07-23 22:54:46 -0400

    python

---
+++ software/python/install_python.py
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+description="install python36"
+echo "enter the directory $(pwd)"
+echo "${description} statr at " $(date +"%Y-%m-%d %H:%M:%S")
+cost_time=$(( end_date - start_date ))
+echo "==================="
+start_date=$(date +"%s")
+
+## python 
+yum -y install python36 && yum -y install python-pip && pip install --upgrade pip
+## link 
+
+end_date=$(date +"%s")
+cost_time=$(( end_date - start_date ))
+echo "==================="
+echo "${description} end at   " $(date +"%Y-%m-%d %H:%M:%S")
+echo "${description} cost_time:"$cost_time"s"
+echo "leave the: directory $(pwd)"
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
