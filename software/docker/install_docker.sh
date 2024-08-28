1a603da1d01596b08d3c56bb64a335deeab119b5
sunfeilong <sunfeilong1993@gmail.com>
2024-08-28 17:34:48 +0800

    update docker

--- software/docker/install_docker.sh
+++ software/docker/install_docker.sh
@@ -9,15 +9,14 @@ start_date=$(date +"%s")
 
 # remove 
 yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-# install  
-# install docker-ce
-yum install -y device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce
 
-# install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# install
+yum install -y yum-utils
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 
 end_date=$(date +"%s")
 cost_time=$(( end_date - start_date ))
